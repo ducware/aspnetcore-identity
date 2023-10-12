9e56b764c52076410c4b8294f20758b0c2d3722b
Dang Minh Duc <112186500+ducware@users.noreply.github.com>
2023-10-12 23:42:18 +0700

    feat: api test role

--- Identity/Controllers/WeatherForecastController.cs
+++ Identity/Controllers/WeatherForecastController.cs
@@ -1,11 +1,12 @@
 using Identity.Constant;
+using Identity.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Identity.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("test-role")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -20,8 +21,7 @@ namespace Identity.Controllers
             _logger = logger;
         }
 
-        [Authorize(Roles = RoleConst.Admin)]
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet("no-role")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
@@ -32,5 +32,51 @@ namespace Identity.Controllers
             })
             .ToArray();
         }
+
+        [Authorize]
+        [HttpGet("all-role")]
+        public async Task<IActionResult> AllRole()
+        {
+            return StatusCode(StatusCodes.Status200OK, new Response
+            {
+                Status = ResponseConst.Success,
+                Message = "ALL ROLE AUTHORIZE"
+            });
+        }
+
+        [Authorize(Roles = RoleConst.Admin)]
+        [HttpGet("role-admin")]
+        public async Task<IActionResult> RoleAdmin()
+        {
+            return StatusCode(StatusCodes.Status200OK, new Response
+            {
+                Status = ResponseConst.Success,
+                Message = "ROLE ADMIN AUTHORIZE"
+            });
+        }
+
+        [Authorize(Roles = RoleConst.Customer)]
+        [HttpGet("role-customer")]
+        public async Task<IActionResult> RoleCustomer()
+        {
+            return StatusCode(StatusCodes.Status200OK, new Response
+            {
+                Status = ResponseConst.Success,
+                Message = "ROLE CUSTOMER AUTHORIZE"
+            });
+        }
+
+        [Authorize(Roles = RoleConst.User)]
+        [HttpGet("role-user")]
+        public async Task<IActionResult> RoleUser()
+        {
+            return StatusCode(StatusCodes.Status200OK, new Response
+            {
+                Status = ResponseConst.Success,
+                Message = "ROLE USER AUTHORIZE"
+            });
+        }
+
+
     }
 }
