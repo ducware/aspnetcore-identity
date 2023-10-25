41d57092a9a48246e9db33cb39b1a2855e3e18f9
Đặng Minh Đức <ducware@gmail.com>
2023-10-25 15:01:12 +0700

    add check valid token middeware

---
+++ Identity/Middlewares/ValidateJwtTokenMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace Identity.Middlewares
+{
+    public class ValidateJwtTokenMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ValidateJwtTokenMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string token = context.Request.Headers["Authorization"].SingleOrDefault()?.Split(" ").Last();
+
+            if (token != null)
+            {
+                try
+                {
+                    var handler = new JwtSecurityTokenHandler();
+                    var jwtToken = handler.ReadToken(token) as JwtSecurityToken;
+
+                    if (jwtToken != null && jwtToken.ValidTo <= DateTime.UtcNow)
+                    {
+                        context.Response.StatusCode = 401;  // Unauthorized
+                        return;
+                    }
+                }
+                catch
+                {
+                    context.Response.StatusCode = 401;  // Unauthorized
+                    return;
+                }
+            }
+
+            await _next.Invoke(context);
+        }
+    }
+}
--- Identity/Program.cs
+++ Identity/Program.cs
@@ -1,5 +1,5 @@
+using Identity.Middlewares;
 using Identity.Services;
-using Services.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +21,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ValidateJwtTokenMiddleware>();
 
 app.UseHttpsRedirection();
 
